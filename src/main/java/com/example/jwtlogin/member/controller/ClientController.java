9fe606fe24974bd45cdc44760f8262ec84b69f65
haeni <haeni.you@aizen.co>
2024-06-19 09:57:10 +0900

    feat(member): 메인 화면 이동 추가

--- src/main/java/com/example/jwtlogin/member/controller/ClientController.java
+++ src/main/java/com/example/jwtlogin/member/controller/ClientController.java
@@ -1,11 +1,13 @@
 package com.example.jwtlogin.member.controller;
 
+import com.example.jwtlogin.common.dto.ResponseDto;
 import com.example.jwtlogin.member.dto.request.MemberLoginRequestDto;
 import com.example.jwtlogin.member.dto.request.MemberSaveRequestDto;
 import com.example.jwtlogin.member.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientController {
 
     private final MemberService memberService;
+
+    @GetMapping(value = "/")
+    public ResponseEntity<?> getMain() {
+        return ResponseEntity.ok(ResponseDto.builder()
+                .result(true)
+                .status(HttpServletResponse.SC_OK)
+                .build());
+    }
 
     @PostMapping(value = "/join")
     public ResponseEntity<?> saveMember(@RequestBody MemberSaveRequestDto memberSaveRequestDto) {
